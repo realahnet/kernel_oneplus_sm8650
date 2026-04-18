1a8238ce1d6bdafec1509a7f6fe8ff39e7eefbac
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2026-04-18 14:50:03 +0600

    sched/fair: Fix type mismatch between vlag and zero integer in update_entity_lag()
    vlag is s64, but 0 by default is int. So, there's a type mismatch. Cast both to s64
    using min_t() to fix the following warning:
    
    ../kernel/sched/fair.c:837:11: warning: comparison of distinct pointer types ('typeof (vlag) *' (aka 'long long *') and 'typeof (0) *' (aka 'int *')) [-Wcompare-distinct-pointer-types]
      837 |                         vlag = min(vlag, 0);
          |                                ^~~~~~~~~~~~
    ../include/linux/kernel.h:903:19: note: expanded from macro 'min'
      903 | #define min(x, y)       __careful_cmp(x, y, <)
          |                         ^~~~~~~~~~~~~~~~~~~~~~
    ../include/linux/kernel.h:894:24: note: expanded from macro '__careful_cmp'
      894 |         __builtin_choose_expr(__safe_cmp(x, y), \
          |                               ^~~~~~~~~~~~~~~~
    ../include/linux/kernel.h:884:4: note: expanded from macro '__safe_cmp'
      884 |                 (__typecheck(x, y) && __no_side_effects(x, y))
          |                  ^~~~~~~~~~~~~~~~~
    ../include/linux/kernel.h:870:29: note: expanded from macro '__typecheck'
      870 |                 (!!(sizeof((typeof(x) *)1 == (typeof(y) *)1)))
          |                            ~~~~~~~~~~~~~~ ^  ~~~~~~~~~~~~~~
    
    Fixes: ff9bef08699 ("FROMLIST: sched/fair: Prevent negative lag increase during delayed dequeue")
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -871,7 +871,7 @@ bool update_entity_lag(struct cfs_rq *cfs_rq, struct sched_entity *se)
 		/* previous vlag < 0 otherwise se would not be delayed */
 		vlag = max(vlag, se->vlag);
 		if (sched_feat(DELAY_ZERO))
-			vlag = min(vlag, 0);
+			vlag = min_t(s64, vlag, 0);
 	}
 	ret = (vlag == se->vlag);
 	se->vlag = vlag;
